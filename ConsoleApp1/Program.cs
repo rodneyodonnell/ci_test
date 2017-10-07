0d5d95b8d0bc43d064a17626d01f34ab34e03300
Rodney O'Donnell <rodo@rome2rio.com>
2017-10-08 01:32:58 +1100

    stash4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World 3 - branch3 - part 2 + web edit.");
             Console.WriteLine("Hello World 3 - with extra branch2 goodness");
+            Console.WriteLine("Hello World 3 - with extra branch4 goodness");
         }
     }
 }
